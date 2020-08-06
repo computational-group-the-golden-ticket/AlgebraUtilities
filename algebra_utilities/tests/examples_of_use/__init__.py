f5a3db61cedc72534306e25c622da9bdd7ce285a
Andresfgomez970 <andresf.gomez970@gmail.com>
2020-08-05 23:22:23 -0500

    fix: good relative path in order to be runned from tests and importing from algebra utilities, this __init__.py will be unecesary when used as a library

--- algebra_utilities/tests/examples_of_use/__init__.py
+++ algebra_utilities/tests/examples_of_use/__init__.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-__from_actual_to_dir__ = "../.."
+__from_actual_to_dir__ = "../../.."
 sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__from_actual_to_dir__),
                                  os.path.pardir)))
